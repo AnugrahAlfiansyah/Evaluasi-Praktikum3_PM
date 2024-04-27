4f0ca75c9f782434350ca216833e48dcfe434a1f
AnugrahAlfiansyah <151117180+AnugrahAlfiansyah@users.noreply.github.com>
2024-04-27 15:57:28 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "A1-Prak2-13120220025"
+include(":app")
